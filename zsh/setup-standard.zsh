fbffcde3721cfe3e3433b8cc4ef652ed2a5144ad
Timóteo Ramos <timoteoramos@ymail.com>
2020-10-18 11:15:22 -0300

    Zshell

---
+++ zsh/setup-standard.zsh
@@ -0,0 +1,11 @@
+#!/bin/zsh
+
+zinit ice compile'(pure|async).zsh' pick'async.zsh' src'pure.zsh'
+zinit light sindresorhus/pure
+zstyle :prompt:pure:path color white
+
+zinit ice blockf
+zinit light zsh-users/zsh-completions
+
+zinit load zdharma/history-search-multi-word
+zinit light zdharma/fast-syntax-highlighting
